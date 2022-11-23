881cad1223cd84c241567724ed5a0db44b8f4234
bhavana2834 <bhavanauxworkz@gmail.com>
2022-11-23 20:50:13 +0530

    committing Java files

---
+++ Trees.java
@@ -0,0 +1,9 @@
+class Trees{
+
+      // variables and datatypes
+	  public static void main(String Trees[]){
+	          String treeName="Mango";
+			  
+	     System .out.println("Different means of Transport are---"+treeName);
+}
+}
