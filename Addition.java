08daaad3fa50d461bd7aa7ed1a30ecd1c62622fd
bhavana2834 <bhavanauxworkz@gmail.com>
2022-12-06 15:57:31 +0530

    adding the new files

---
+++ Addition.java
@@ -0,0 +1,23 @@
+class Addition{
+
+	public static void main(String add[]){
+	      add(87933,97562);
+		  add(786645,914687);
+		  add(07325,49145);
+		  add(86437,68533);
+		  add(8663,69675);
+		  add(15546,676,887);
+	}
+    public   static int add(int number1, int number2){
+	
+	System.out.println(number1+number2);
+	return number1+number2;
+	}
+		public static int add(int number1,int number2,int number3){
+			System.out.println(number1+number2+number3);
+			return number1+number2+number3;
+			
+		}
+
+
+}
