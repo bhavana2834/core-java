4964e4040e55495d84e9c453e6a9ab951cfd28e8
bhavana2834 <bhavanauxworkz@gmail.com>
2023-03-14 19:24:38 +0530

    uploading stream files

---
+++ functional -management-system/src/com/xworkz/functionalapp/functionalInterface/Addition.java
@@ -0,0 +1,13 @@
+package com.xworkz.functionalapp.functionalInterface;
+
+
+@FunctionalInterface
+
+public interface Addition {
+	
+	
+	
+	void add(int a,int b);
+	
+              
+}
---
+++ functional -management-system/src/com/xworkz/functionalapp/functionalInterface/Division.java
@@ -0,0 +1,13 @@
+package com.xworkz.functionalapp.functionalInterface;
+
+
+
+@FunctionalInterface
+
+
+public interface Division {
+
+	
+	void div(int a,int b);
+	
+}
